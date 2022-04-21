62f81d77ff4a1386537a8ac1db0a1c833b7f896b
Bimzking1 <kingofbimo@gmail.com>
2022-04-21 11:28:35 +0700

    Minishop Part 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/products" element={<h1>Products</h1>} />
+          <Route path="/categories" element={<h1>Categories</h1>} />
+          <Route path="/shopping-cart" element={<h1>Shopping Cart</h1>} />
+          <Route path="/order-history" element={<h1>Order History</h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
